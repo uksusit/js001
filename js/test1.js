a12b18d7e78ca29e2b21ea8e9d3f14500e5344ba
uksusit <140089654+uksusit@users.noreply.github.com>
2024-03-31 20:27:43 +0300

    number

---
+++ js/test1.js
@@ -0,0 +1,12 @@
+// const x = 0;
+// console.log(x);
+// const type = typeof x;
+// console.log(type);
+
+
+
+let wtf = prompt('що трапилось?');
+wtf = Number(wtf);
+console.log('сталося такє:', wtf);
+console.log(typeof wtf);
+
