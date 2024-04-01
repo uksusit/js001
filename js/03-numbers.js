6dd609136762e08d002bd84c1511cc90d4802162
uksusit <140089654+uksusit@users.noreply.github.com>
2024-04-01 19:46:50 +0300

    toFixed

--- js/03-numbers.js
+++ js/03-numbers.js
@@ -1,9 +1,14 @@
 
-let elementWidth = '50px';
-const result = Number.parseInt(elementWidth);
-console.log(result);
+// let elementWidth = '50px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
 
-let elementHeight = '5655.2226px';
-elementHeight = Number.parseFloat(elementHeight);
-console.log('elementHeight:', elementHeight);
+// let elementHeight = '5655.2226px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log('elementHeight:', elementHeight);
+
+let salary = 64654.226589;
+salary = salary.toFixed(2);
+salary = Number(salary);
+console.log(salary);
 
