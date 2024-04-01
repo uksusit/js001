ed5bd3f4de778c12d1d1f943b3758dea7125c549
uksusit <140089654+uksusit@users.noreply.github.com>
2024-04-01 20:13:27 +0300

    pow

--- js/03-numbers.js
+++ js/03-numbers.js
@@ -7,8 +7,23 @@
 // elementHeight = Number.parseFloat(elementHeight);
 // console.log('elementHeight:', elementHeight);
 
-let salary = 64654.226589;
-salary = salary.toFixed(2);
-salary = Number(salary);
-console.log(salary);
+
+
+// let salary = 64654.226589;
+// salary = salary.toFixed(2);
+// salary = Number(salary);
+// console.log(salary);
+
+// let salary = 64654.226589;
+// salary = Number(salary.toFixed(2));
+// // salary = Number(salary);
+// console.log(salary);
+
+
+// const base = 10;
+// const power = 3;
+// const result = Math.pow(base, power);
+// console.log(result);
+
+console.log(10 ** 3);
 
