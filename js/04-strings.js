a77ff360998c690c8cca81bd1395d01258436a70
uksusit <140089654+uksusit@users.noreply.github.com>
2024-04-02 20:50:21 +0300

    toLowerCase

--- js/04-strings.js
+++ js/04-strings.js
@@ -1,3 +1,20 @@
 const message = 'тут нцть символів)))(_._))----';
 console.log(message);
 console.log('---',message.length);
+
+
+
+const quantity = 15;
+const orderMsg = `вибрано ${quantity} ківі`;
+console.log(orderMsg);
+
+
+let brand = 'KyoCeRa';
+console.log(brand);
+console.log(brand[3]);
+console.log(brand.slice(1).toLowerCase());
+console.log(brand);
+brand = brand[0] + brand.slice(1).toLowerCase();
+console.log(brand);
+
+
