1565e19b0beeee3695475493cf86cde7d916722a
uksusit <140089654+uksusit@users.noreply.github.com>
2024-04-22 22:10:26 +0300

    34

--- js/04-strings.js
+++ js/04-strings.js
@@ -35,3 +35,21 @@ const normalizedString2 = string2.toLowerCase();
 console.log(normalizedString2.includes(blWord2));
 
 
+
+
+
+// 34 check
+
+
+
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase(); // Change this line
+//   return normalizedInput;
+// }
+
+// Функція normalizeInput(input) приймає рядок (параметр input) і повертає такий самий рядок, але в нижньому регістрі. Присвой змінній normalizedInput вираз створення рядка у нижньому регістрі з параметра input.
+
+// Оголошена функція normalizeInput(input)
+// Виклик функції normalizeInput("Hello world") повертає "hello world"
+// Виклик функції normalizeInput("This ISN'T SpaM") повертає "this isn't spam"
+// Виклик функції normalizeInput("Big SALE") повертає "big sale"
