15ce41a087c82bb3ac2e053266ff6cc6b2bacfb4
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-04 19:13:52 +0300

    a2.19

--- js/a2.18.js
+++ js/a2.18.js
@@ -19,3 +19,24 @@ for (let i = 1; i <= number; i += 1) {
 return sum;
   // Change code above this line
 }
+
+
+
+// Доповни код циклу for таким чином, щоб він послідовно логував усі елементи масиву fruits.
+
+// Оголошена змінна fruits
+// Значення змінної fruits - це масив ["apple", "plum", "pear", "orange"]
+// Оголошена змінна i - лічильник циклу
+// Початкове значення змінної i дорівнює 0
+// Умова циклу приводиться до true доти, доки i менше за 4
+// На кожній ітерації значення змінної i збільшується на одиницю
+// В тілі циклу for оголошується змінна const fruit і цій змінній присвоюється значення - елемент масиву
+// В тілі циклу for використовується виведення у консоль змінної fruit
+
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i < fruits.length;  i += 1) { // Change this line
+  const fruit = fruits[i]; // Change this line
+  console.log(fruit);
+}
