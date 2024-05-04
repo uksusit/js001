77afb8cebaa8b7775fc61b45a1da84f13126825e
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-04 18:47:20 +0300

    2.16ok

--- js/a2.18.js
+++ js/a2.18.js
@@ -1,12 +1,21 @@
 // function calculateTotal(number) {
-// Change code below this line
-const number = 8;
-const sum = 0;
-for (let i = 0; i <= number; i += 1) {
-    sum = sum + i;
-    console.log(sum);
-}
-// return
-console.log("sum=", sum);
-  // Change code above this line
+// const number = 64;
+// let sum = 0;
+// for (let i = 0; i <= number; i += 1) {
+//     sum += i;
+//     console.log(sum);
 // }
+// console.log("sum=", sum);
+
+
+// Напиши функцію calculateTotal(number), яка приймає ціле число (параметр number) і повертає суму всіх цілих чисел від одиниці і до цього числа. Наприклад, якщо number дорівнює 3, то сума - це 1 + 2 + 3, тобто 6.
+
+function calculateTotal(number) {
+ // Change code below this line
+let sum = 0;
+for (let i = 1; i <= number; i += 1) {
+  sum += i;
+}
+return sum;
+  // Change code above this line
+}
