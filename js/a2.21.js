fac28199c17af19c9fafef0f66c5b9133ee979b1
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-06 20:48:42 +0300

    a21

--- js/a2.21.js
+++ js/a2.21.js
@@ -12,3 +12,36 @@
 // Виклик функції findLongestWord("May the force be with you") повертає force
 // Виклик функції findLongestWord() з випадковим рядком повертає правильне значення
 
+const string = ("The quick brown fox jumped over the lazy dog");
+  // Change code below this line
+
+  const strArr = string.split(" ");
+console.log(strArr);
+  let longestWord = strArr[0];
+console.log("init word", longestWord);
+      console.log("array l=", strArr.length);
+  for (let i = 1; i < strArr.length; i += 1){
+      console.log("i (word number) =", i);
+      console.log("word length  = ", strArr[i].length);
+      console.log(strArr[i]);
+      if (longestWord.length < strArr[i].length) {
+      longestWord = strArr[i];
+    console.log(" --------------      longest = ", longestWord);
+      }   
+}
+console.log("end.  -----------   longest = ", longestWord);
+    
+
+    //  --------------------   a21
+// function findLongestWord(string) {
+//   // Change code below this line
+//   const strArr = string.split(" ");
+//   let longestWord = strArr[0];
+//   for (let i = 1; i < strArr.length; i += 1){
+//     if (longestWord.length < strArr[i].length) {
+//       longestWord = strArr[i];
+//     }
+//   }
+// return longestWord;
+//   // Change code above this line
+// }
