565687832f18acf8378752b42ef6400e1c07dc55
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-09 20:56:20 +0300

    a25

--- js/a2.23.js
+++ js/a2.23.js
@@ -32,3 +32,30 @@ function filterArray(numbers, value) {
 
 ;
 console.log(filterArray([12, 24, 8, 41, 76], 38));
+
+
+
+// 24 -----------------
+
+// Функція checkFruit(fruit) приймає рядок з назвою фрукта (параметр fruit), і перевіряє, чи присутній такий фрукт в масиві fruits.
+
+// Доповни код функції таким чином, що якщо:
+
+// фрукт присутній в масиві, то функція повертає true;
+// фрукт відсутній в масиві, то функція повертає false.
+// Оголошена функція checkFruit(fruit)
+// Виклик checkFruit("plum") повертає true
+// Виклик checkFruit("mandarin") повертає false
+// Виклик checkFruit("pear") повертає true
+// Виклик checkFruit("Pear") повертає false
+// Виклик checkFruit("apple") повертає true
+// Виклик функції checkFruit() з випадковим словом повертає правильне значення boolean
+// У функції використовувався метод includes
+        
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit); // Change this line
+}
+
+console.log(checkFruit("mandarin"));
