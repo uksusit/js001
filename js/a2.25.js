c1e0a3128d8eb8a413872d2ac633abfdbbc1bdc1
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-09 21:17:35 +0300

    a27

--- js/a2.25.js
+++ js/a2.25.js
@@ -61,3 +61,28 @@ function calculateTotalPrice(order) {
 }
 
 console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+
+// ---------------------  27  ----------------------
+        
+function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   const number = numbers[i];
+  
+  for (let number of numbers) {
+    
+  // }
+  
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+console.log(filterArray([12, 24, 8, 41, 76], 20));
