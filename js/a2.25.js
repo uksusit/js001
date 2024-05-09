d6fb6d4a2badca2f39b26348ff608dd7261852eb
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-09 21:10:27 +0300

    a26

--- js/a2.25.js
+++ js/a2.25.js
@@ -33,3 +33,31 @@ function getCommonElements(array1, array2) {
 }
 
 console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+
+// -------------  26  --------------
+// Виконай рефакторинг коду функції calculateTotalPrice(order), замінивши цикл for на for...of.
+
+// Оголошена функція calculateTotalPrice(order)
+// Виклик функції calculateTotalPrice([12, 85, 37, 4]) повертає 138
+// Виклик функції calculateTotalPrice([164, 48, 291]) повертає 503
+// Виклик функції calculateTotalPrice([412, 371, 94, 63, 176]) повертає 1116
+// Виклик функції calculateTotalPrice([]) повертає 0
+// Виклик функції calculateTotalPrice() з випадковим масивом чисел повертає правильну суму
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+
+  // for (let i = 0; i < order.length; i += 1) {
+    //   total += order[i];
+    
+    for (const item of order) {
+      
+    total += item;
+  }
+
+  // Change code above this line
+  return total;
+}
+
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
