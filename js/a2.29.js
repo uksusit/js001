442ed582b2420709b71efa373512086475ffe1ec
uksusit <140089654+uksusit@users.noreply.github.com>
2024-05-10 20:56:24 +0300

    a2.31

--- js/a2.29.js
+++ js/a2.29.js
@@ -23,3 +23,26 @@ function getEvenNumbers(start, end) {
     // Change code above this line
 }
 console.log(getEvenNumbers(3, 11));
+
+
+
+// ---------------- 30  ----------
+
+// Доповни код таким чином, щоб у змінну number записувалося перше число від start до end, яке ділиться на 5 без остачі.
+
+// Оголошена змінна start зі значенням 6
+// Оголошена змінна end зі значенням 27
+// Оголошена змінна number без ініціалізації
+// Підсумкове значення змінної number дорівнює 10
+// В циклі for використовується break для виходу до завершення усіх ітерацій циклу
+
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+  if (i % 5 === 0) {
+    number = i;
+ break;
+  }
+}
